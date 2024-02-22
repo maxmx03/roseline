782b0cd7e3c6177f9ca7655b148ef4244c77d69c
max miliano <milianordelcanto@gmail.com>
2024-02-22 10:24:26 -0300

    feat: dracula-theme

---
+++ lua/roseline/themes/dracula.lua
@@ -0,0 +1,11 @@
+return {
+  background = '#282A36',
+  gray = '#6272A4',
+  foreground = '#F8F8F2',
+  red = '#FF5555',
+  yellow = '#F1FA8C',
+  pink = '#FF79C6',
+  green = '#50FA7B',
+  cyan = '#8BE9FD',
+  purple = '#BD93F9',
+}
